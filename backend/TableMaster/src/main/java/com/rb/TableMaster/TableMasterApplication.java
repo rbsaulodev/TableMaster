dd5ec8885c7a30535413e119318d5808263d40f1
rbsaulodev <rb.saulodev@gmail.com>
2025-05-13 09:58:28 -0300

    chore: inicia projeto TableMaster com estrutura base

---
+++ backend/TableMaster/src/main/java/com/rb/TableMaster/TableMasterApplication.java
@@ -0,0 +1,13 @@
+package com.rb.TableMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableMasterApplication.class, args);
+	}
+
+}
---
+++ backend/TableMaster/src/test/java/com/rb/TableMaster/TableMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rb.TableMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
